d56f0249d1f46a2dec1b0171d56bbaaf1d294583
Rob van Geloven <rob@vangeloven.net>
2024-12-04 15:28:49 +0100

    Day 4

---
+++ Day4/BufferMatrix.cs
@@ -0,0 +1,27 @@
+﻿namespace Day4;
+
+internal class BufferMatrix
+{
+    private readonly IEnumerable<CircularBuffer> _buffers;
+
+    public BufferMatrix(IEnumerable<CircularBuffer> buffers)
+    {
+        _buffers = buffers;
+    }
+
+    public int CountWord(string word)
+    {
+        var count = 0;
+
+        //for (var i = 0; i < _buffers.First().Length; i++)
+        //{
+        //    count += _buffers.Select(buffer =>
+        //    {
+        //        buffer.Shift(i);
+        //        return buffer.CountWord(word);
+        //    }).Sum();
+        //}
+
+        return count;
+    }
+}
--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,8 +1,103 @@
-﻿var input = await File.ReadAllTextAsync("input.txt");
+﻿var input = await File.ReadAllLinesAsync("input.txt");
+var matrix = input.Select(line => line.ToArray()).ToArray();
 
 void PartOne()
 {
     var answer = 0;
+
+    var width = matrix[0].Length;
+    var height = matrix.Length;
+
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            if (matrix[y][x] != 'X')
+            {
+                continue;
+            }
+
+            if (x <= width - 4)
+            {
+                if (matrix[y][x + 1] == 'M' &&
+                    matrix[y][x + 2] == 'A' &&
+                    matrix[y][x + 3] == 'S')
+                {
+                    answer++;
+                }
+
+                if (y <= height - 4)
+                {
+                    if (matrix[y + 1][x + 1] == 'M' &&
+                        matrix[y + 2][x + 2] == 'A' &&
+                        matrix[y + 3][x + 3] == 'S')
+                    {
+                        answer++;
+                    }
+                }
+
+                if (y >= 3)
+                {
+                    if (matrix[y - 1][x + 1] == 'M' &&
+                        matrix[y - 2][x + 2] == 'A' &&
+                        matrix[y - 3][x + 3] == 'S')
+                    {
+                        answer++;
+                    }
+                }
+            }
+
+            if (x >= 3)
+            {
+                if (matrix[y][x - 1] == 'M' &&
+                    matrix[y][x - 2] == 'A' &&
+                    matrix[y][x - 3] == 'S')
+                {
+                    answer++;
+                }
+
+                if (y <= height - 4)
+                {
+                    if (matrix[y + 1][x - 1] == 'M' &&
+                        matrix[y + 2][x - 2] == 'A' &&
+                        matrix[y + 3][x - 3] == 'S')
+                    {
+                        answer++;
+                    }
+                }
+
+                if (y >= 3)
+                {
+                    if (matrix[y - 1][x - 1] == 'M' &&
+                        matrix[y - 2][x - 2] == 'A' &&
+                        matrix[y - 3][x - 3] == 'S')
+                    {
+                        answer++;
+                    }
+                }
+            }
+
+            if (y <= height - 4)
+            {
+                if (matrix[y + 1][x] == 'M' &&
+                    matrix[y + 2][x] == 'A' &&
+                    matrix[y + 3][x] == 'S')
+                {
+                    answer++;
+                }
+            }
+
+            if (y >= 3)
+            {
+                if (matrix[y - 1][x] == 'M' &&
+                    matrix[y - 2][x] == 'A' &&
+                    matrix[y - 3][x] == 'S')
+                {
+                    answer++;
+                }
+            }
+        }
+    }
 
     Console.WriteLine($"Answer part one: {answer}");
 }
@@ -10,6 +105,30 @@ void PartOne()
 void PartTwo()
 {
     var answer = 0;
+
+    var width = matrix[0].Length;
+    var height = matrix.Length;
+
+    for (var y = 1; y < height - 1; y++)
+    {
+        for (var x = 1; x < width - 1; x++)
+        {
+            if (matrix[y][x] is not 'A')
+            {
+                continue;
+            }
+
+            if (((matrix[y - 1][x - 1] is 'M' && matrix[y + 1][x + 1] is 'S') ||
+                (matrix[y - 1][x - 1] is 'S' && matrix[y + 1][x + 1] is 'M'))
+                &&
+                ((matrix[y + 1][x - 1] is 'M' && matrix[y - 1][x + 1] is 'S') ||
+                (matrix[y + 1][x - 1] is 'S' && matrix[y - 1][x + 1] is 'M')))
+            {
+                answer++;
+                continue;
+            }
+        }
+    }
 
     Console.WriteLine($"Answer part two: {answer}");
 }
