ac26a9bf98251af958d78fb97eb279a887bb2400
Rob van Geloven <rob@vangeloven.net>
2024-12-04 15:39:09 +0100

    Day5 bootstrap

---
+++ Day5/Program.cs
@@ -0,0 +1,21 @@
+﻿var input = await File.ReadAllLinesAsync("input.txt");
+var matrix = input.Select(line => line.ToArray()).ToArray();
+
+void PartOne()
+{
+    var answer = 0;
+
+    Console.WriteLine($"Answer part one: {answer}");
+}
+
+void PartTwo()
+{
+    var answer = 0;
+
+    Console.WriteLine($"Answer part two: {answer}");
+}
+
+PartOne();
+PartTwo();
+
+Console.ReadLine();
