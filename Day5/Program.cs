b31eba321bce77daa183b2e17dcaf210d3eecbaf
Rob van Geloven <rob@vangeloven.net>
2024-12-07 22:02:43 +0100

    Belated Day 5

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,32 +1,61 @@
-﻿//var input = await File.ReadAllLinesAsync("input.txt");
+﻿var input = File.ReadAllLines("input.txt");
+var rules = new Dictionary<int, List<int>>();
+List<List<int>> manuals = [];
 
-var input = File.OpenText("input.txt");
-string? line;
-var instructions = new Dictionary<int, List<int>>();
+var instructionProcessing = true;
 
-do
+foreach (var line in input)
 {
-    line = input.ReadLine();
-
-    if (line is null)
+    if (string.IsNullOrWhiteSpace(line))
     {
+        instructionProcessing = false;
         continue;
     }
 
-    var values = line.Split('|').Select(int.Parse).ToList();
-
-    if (!instructions.ContainsKey(values[0]))
+    if (instructionProcessing)
     {
-        instructions.Add(values[0], []);
+        var values = line.Split('|').Select(int.Parse).ToArray();
+
+        if (!rules.ContainsKey(values[0]))
+        {
+            rules[values[0]] = [];
+        }
+
+        rules[values[0]].Add(values[1]);
     }
+    else
+    {
+        manuals.Add([.. line.Split(',').Select(int.Parse)]);
+    }
+}
 
-    instructions[values[0]].Add(values[1]);
+bool IsValidPage(IList<int> pages)
+{
+    return rules.All(ruleSet =>
+    {
+        var ruleIndex = pages.IndexOf(ruleSet.Key);
 
-} while (!string.IsNullOrWhiteSpace(line));
+        if (ruleIndex < 0)
+        {
+            return true;
+        }
+
+        return ruleSet.Value.All(rule =>
+        {
+            var pageIndex = pages.IndexOf(rule);
+
+            return pageIndex < 0
+            ? true
+            : ruleIndex < pageIndex;
+        });
+    });
+}
 
 void PartOne()
 {
     var answer = 0;
+
+    answer = manuals.Sum(manual => IsValidPage(manual) ? manual[(manual.Count - 1) / 2] : 0);
 
     Console.WriteLine($"Answer part one: {answer}");
 }
@@ -34,6 +63,33 @@ void PartOne()
 void PartTwo()
 {
     var answer = 0;
+
+    answer = manuals.Sum(manual =>
+    {
+        if (IsValidPage(manual))
+        {
+            return 0;
+        }
+
+        manual.Sort((pageA, pageB) =>
+        {
+            if (!rules.TryGetValue(pageA, out var pageARules))
+            {
+                return 1;
+            }
+
+            if (!rules.TryGetValue(pageB, out var pageBRules))
+            {
+                return -1;
+            }
+
+            return pageARules.Contains(pageB)
+            ? -1
+            : 1;
+        });
+
+        return manual[(manual.Count - 1) / 2];
+    });
 
     Console.WriteLine($"Answer part two: {answer}");
 }
