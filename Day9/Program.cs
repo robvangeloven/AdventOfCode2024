a1b1defe50411c8517fa6a8c837f9e99801ce313
Rob van Geloven <rob@vangeloven.net>
2024-12-09 23:08:48 +0100

    Day9 part 1

---
+++ Day9/Program.cs
@@ -0,0 +1,49 @@
+﻿
+void PartOne()
+{
+    var emptyNodes = 0;
+    var stack = new Stack<int>();
+
+    var answer = File
+        .ReadAllText("input.txt")
+        .SelectMany((c, index) =>
+        {
+            var fileId = index / 2;
+
+            var result = Enumerable
+               .Range(0, int.Parse($"{c}"))
+               .Select(x =>
+               {
+                   if (index % 2 == 0)
+                   {
+                       stack.Push(fileId);
+                       return fileId;
+                   }
+
+                   emptyNodes++;
+                   return (int?)null;
+               });
+
+            return result;
+
+        })
+        .ToArray()
+        .Select(node => node ?? stack.Pop())
+        .Take(..^emptyNodes)
+        .Select((value, index) => index * (long)value)
+        .Sum();
+
+    Console.WriteLine($"Answer part one: {answer}");
+}
+
+void PartTwo()
+{
+    var answer = 0;
+
+    Console.WriteLine($"Answer part two: {answer}");
+}
+
+PartOne();
+PartTwo();
+
+Console.ReadLine();
